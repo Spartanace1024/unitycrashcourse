5e8a94de428ba8dbc084910b092d97e99e636337
Thomas <spartanace1024@gmail.com>
2018-12-17 15:17:38 -0800

    Create Actual Game

---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Shake : MonoBehaviour {
+    public float shakeAmount;	
+    
+    private Vector3 originalPos;
+	
+    void Awake() {
+        originalPos = transform.localPosition;
+    }
+
+    void Update() {
+        if (shakeAmount > 0) {
+            transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+        } else {
+            transform.localPosition = originalPos;
+        }
+    }
+}
