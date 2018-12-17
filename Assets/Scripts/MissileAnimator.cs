9eadc44a6d0e600cc4a5dfd5635a16162610436e
Thomas <spartanace1024@gmail.com>
2018-12-17 12:24:49 -0800

    Add turning and explosion animations

--- Assets/Scripts/MissileAnimator.cs
+++ Assets/Scripts/MissileAnimator.cs
@@ -3,8 +3,6 @@
 public class MissileAnimator : MonoBehaviour {
     public Animator missileAnimator;
     public ThrustController thrustController;
-    public ParticleSystem leftThrustParticleSystem;
-    public ParticleSystem rightThrustParticleSystem;
     
     // Start is called before the first frame update
     void Start() {
@@ -13,15 +11,9 @@ public class MissileAnimator : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //really no easy dependable way via editor to start and stop particle system
-        leftThrustParticleSystem.Stop();
-        rightThrustParticleSystem.Stop();
-        if (thrustController.IsLeftThrust) {
-            leftThrustParticleSystem.Play();
-        }    
-        if (thrustController.IsRightThrust) {
-            rightThrustParticleSystem.Play();
-        }    
+        missileAnimator.SetBool("isRightThrust",thrustController.IsRightThrust);
+        missileAnimator.SetBool("isLeftThrust",thrustController.IsLeftThrust);
         missileAnimator.SetBool("isBoosting",thrustController.IsBoosting);
+        missileAnimator.SetTrigger("trigExplode");
     }
 }
