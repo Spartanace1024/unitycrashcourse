e7785cc55226cf0cbdaf051a1d7497437cec32c7
Thomas <spartanace1024@gmail.com>
2018-12-17 13:47:25 -0800

    Add Satellites

--- Assets/Scripts/CollisionBroadcaster.cs
+++ Assets/Scripts/CollisionBroadcaster.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionBroadcaster : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class CollisionBroadcaster : MonoBehaviour {
+    public event Action<GameObject> OnCollision;
+    public List<string> validCollisionTags;
+    
+    private void OnCollisionEnter2D(Collision2D collision) {
+        foreach (var tag in validCollisionTags) {
+            if (collision.gameObject.CompareTag(tag)) {
+                OnCollision?.Invoke(collision.gameObject);
+                return;
+            }
+        }
     }
 }
--- Assets/Scripts/MissileAnimator.cs
+++ Assets/Scripts/MissileAnimator.cs
@@ -3,10 +3,16 @@
 public class MissileAnimator : MonoBehaviour {
     public Animator missileAnimator;
     public ThrustController thrustController;
-    
+    public CollisionBroadcaster collisionBroadcaster;
+    public ParticleSystem exhaustParticles;
+
     // Start is called before the first frame update
     void Start() {
-            
+        collisionBroadcaster.OnCollision += Explode;
+    }
+
+    private void OnDisable() {
+        collisionBroadcaster.OnCollision -= Explode;
     }
 
     // Update is called once per frame
@@ -14,6 +20,10 @@ public class MissileAnimator : MonoBehaviour {
         missileAnimator.SetBool("isRightThrust",thrustController.IsRightThrust);
         missileAnimator.SetBool("isLeftThrust",thrustController.IsLeftThrust);
         missileAnimator.SetBool("isBoosting",thrustController.IsBoosting);
+    }
+
+    private void Explode(GameObject enemy) {
         missileAnimator.SetTrigger("trigExplode");
+        exhaustParticles.Stop();
     }
 }
