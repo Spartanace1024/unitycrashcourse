1268bc74f684522a552de3eae1375a7a10cb6ac7
Thomas <spartanace1024@gmail.com>
2018-12-17 11:19:55 -0800

    Initial commit

---
+++ Assets/Scripts/MissileAnimator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MissileAnimator : MonoBehaviour {
+    public Animator missileAnimator;
+    public ThrustController thrustController;
+    public ParticleSystem leftThrustParticleSystem;
+    public ParticleSystem rightThrustParticleSystem;
+    
+    // Start is called before the first frame update
+    void Start() {
+            
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //really no easy dependable way via editor to start and stop particle system
+        leftThrustParticleSystem.Stop();
+        rightThrustParticleSystem.Stop();
+        if (thrustController.IsLeftThrust) {
+            leftThrustParticleSystem.Play();
+        }    
+        if (thrustController.IsRightThrust) {
+            rightThrustParticleSystem.Play();
+        }    
+        missileAnimator.SetBool("isBoosting",thrustController.IsBoosting);
+    }
+}
---
+++ Assets/Scripts/ThrustController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class ThrustController : MonoBehaviour {
+    public float forwardForce;
+    public float addedBoostForce;
+    public float turnForce;
+    public Transform forcePosition;
+    public Transform leftForcePosition;
+    public Transform rightForcePosition;
+    
+    private Rigidbody2D _rigidbody2D;
+    private float horizontalInput;
+    private bool isBoosting;
+    private bool isLeftThrust;
+    private bool isRightThrust;
+
+    // Start is called before the first frame update
+    void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        isBoosting = Input.GetButton("Jump");
+    }
+
+    private void FixedUpdate() {
+        var direction = transform.position - forcePosition.position;
+        var currentForce = forwardForce;
+        
+        if (isBoosting) {
+            currentForce += addedBoostForce;
+        }
+        _rigidbody2D.AddForceAtPosition(direction.normalized*currentForce,forcePosition.position);
+        
+        if (horizontalInput > 0) {
+            _rigidbody2D.AddForceAtPosition(leftForcePosition.up*turnForce,leftForcePosition.position);
+            Debug.Log("Turning Right");
+        }
+        
+        if (horizontalInput < 0) {
+            _rigidbody2D.AddForceAtPosition(rightForcePosition.up*turnForce,rightForcePosition.position);
+            Debug.Log("Turning Left");
+        }
+    }
+
+    public bool IsBoosting {
+        get {
+            return isBoosting;
+        }
+    }
+
+    public bool IsLeftThrust {
+        get {
+            return horizontalInput > 0;
+        }
+    }
+    
+    public bool IsRightThrust {
+        get {
+            return horizontalInput < 0;
+        }
+    }
+
+}
